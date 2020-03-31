cf08452e2e84dd3aba8d62010be2969f8170f7a3
Tyler <vick.tyler@gmail.com>
2020-03-30 22:10:06 -0400

    TV - add triggering agents to redux store

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,12 @@
 import {SET_DATE_FROM_CALENDAR, SET_DATE_FROM_DISPLAY, SET_DATE_FROM_SCROLL} from "../actions/actions";
 import {today} from "../utils/dateDisplayOptions";
 
+export const triggeringAgents = {
+    DATE_DISPLAY: 'DATE_DISPLAY',
+    NEWS_LIST: 'NEWS_LIST',
+    CALENDAR_PICKER: 'CALENDAR_PICKER',
+};
+
 const initialState = {
     viewDate: today().toISOString()
 };
@@ -8,11 +14,20 @@ const initialState = {
 export default function rootReducer(state = initialState, action) {
     switch (action.type) {
         case SET_DATE_FROM_DISPLAY:
-            return Object.assign({}, state, {viewDate: action.viewDate});
+            return Object.assign({}, state, {
+                viewDate: action.viewDate,
+                triggeringAgent: triggeringAgents.DATE_DISPLAY
+            });
         case SET_DATE_FROM_SCROLL:
-            return Object.assign({}, state, {viewDate: action.viewDate});
+            return Object.assign({}, state, {
+                viewDate: action.viewDate,
+                triggeringAgent: triggeringAgents.NEWS_LIST
+            });
         case SET_DATE_FROM_CALENDAR:
-            return Object.assign({}, state, {viewDate: action.viewDate});
+            return Object.assign({}, state, {
+                viewDate: action.viewDate,
+                triggeringAgent: triggeringAgents.CALENDAR_PICKER
+            });
         default:
             return state;
     }
