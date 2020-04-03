21a06578610d09170aa3d2d5d885996dbb3e7445
Tyler <vick.tyler@gmail.com>
2020-04-03 19:01:13 -0400

    display weekly totals in infection chart

--- src/components/sidebar/InfectionCurve.js
+++ src/components/sidebar/InfectionCurve.js
@@ -8,6 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import axios from "axios";
 import axiosHeader from "../../utils/axiosHeader";
 import generateDateString from "../../utils/generateDateString";
+import * as moment from "moment";
 
 const useStyles = makeStyles((theme) => ({
   chartAreaWrapper: {
@@ -22,18 +23,26 @@ const useStyles = makeStyles((theme) => ({
   },
   chartTitle: {
     fontSize: '12px',
-    color: '#000',
+    color: '#757575',
     fontWeight: '700'
+  },
+  tooltipLabel: {
+    fontSize: '10px',
+    margin: '0'
   }
 }));
+
+// primary color: hsl(258, 100%, 50%)
+const infected_text_color = "hsla(243, 100%, 60%)";
+const infected_bar_color = "hsla(233, 100%, 83%)";
+const deaths_bar_color = "red";
+
 
 function InfectionCurve() {
   const classes = useStyles();
   const [chartData, setChartData] = useState([]);
 
-
   useEffect(() => {
-      console.log("useMemo");
       getCaseData()
     }, []);
 
@@ -44,14 +53,16 @@ function InfectionCurve() {
       .then((res) => {
         if (res.status === 200) {
           if (res.data.data) {
-            const newChartData = res.data.data.map((dataPoint) => {
-              // use UTC here
-              return {
-                as_of: dataPoint.as_of,
-                total_infected: dataPoint.sum_total,
-                total_deaths: dataPoint.sum_deaths,
-              }
-            });
+            const newChartData = res.data.data
+              .map((dataPoint) => {
+                return {
+                  as_of_string: generateDateString(dataPoint.as_of, false),
+                  day_of_week: moment.utc(dataPoint.as_of).day(),
+                  total_infected: dataPoint.sum_total,
+                  total_deaths: dataPoint.sum_deaths,
+                }
+              })
+              .filter(dataPoint => dataPoint.day_of_week === 6);
 
             setChartData(newChartData);
           }
@@ -62,37 +73,33 @@ function InfectionCurve() {
       })
   };
 
-  const infected_color = "hsla(240, 50%, 80%)";
-  const infected_color_readable = "hsla(240, 50%, 60%)";
-  const deaths_color = "red";
-
   return (
     <div className={classes.chartAreaWrapper}>
-      <h6 className={classes.chartTitle}>INFECTION CURVE</h6>
+      <h6 className={classes.chartTitle}>WEEKLY INFECTION TOTALS</h6>
       <div className={classes.chartWrapper}>
         <ResponsiveBar
           data={chartData}
-          keys={['total_infected', 'total_deaths']}
-          indexBy="as_of"
+          keys={['total_deaths', 'total_infected']}
+          indexBy="as_of_string"
           margin={{ top: 0, right: 0, bottom: 5, left: 40 }}
           padding={0.3}
-          colors={[infected_color, deaths_color]}
+          colors={[deaths_bar_color, infected_bar_color]}
           enableLabel={false}
           tooltip={({data}) => (
             <div>
-              <p style={{fontSize: '10px', margin: '0'}}>
-                TOTALS AS OF {generateDateString(data.as_of, false)}
+              <p className={classes.tooltipLabel}>
+                AS OF {data.as_of_string}
               </p>
-              <p style={{color: infected_color_readable, fontSize: '10px', margin: '0'}}>
-                INFECTED: {data.total_infected}
+              <p style={{color: infected_text_color}} className={classes.tooltipLabel}>
+                {data.total_infected} INFECTED
               </p>
-              <p style={{color: deaths_color, fontSize: '10px', margin: '0'}}>
-                DEATHS: {data.total_deaths}
+              <p style={{color: deaths_bar_color}} className={classes.tooltipLabel}>
+                {data.total_deaths} DEATHS
               </p>
             </div>
           )
           }
-          xFormat="time:%Y-%m-%d"
+          enableGridX={false}
           enableGridY={true}
           gridYValues={4}
           axisTop={null}
